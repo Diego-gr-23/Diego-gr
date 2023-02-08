43c1a1840c94a111949318d2f7e5e2f0363996ae
Diego-gr-23 <124418381+Diego-gr-23@users.noreply.github.com>
2023-02-07 23:45:51 -0600

    actualizacion de main.py
    agregue una pequeña base de datos, la cual me esta ayudando a practicar.

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print('Luis DIego José Gómez Ramírez')
+nombre1 = input('ingrese su primer nombre:')
+nombre2 = input('ingrese su segundo nombre:')
+apellido1 = input('ingrese su primer apellido:')
+apellido2 = input('ingrese su segundo apellido:')
+fechanaciemiento = input('Ingrese su fecha de naciemiento:')
+facultad = input('En que facultad esta?:')
+carrera = input('Que carrera estudia?:')
+print('Sus datos son los siguientes:')
+print(nombre1, nombre2)
+print(apellido1, apellido2)
+print(fechanaciemiento)
+print(facultad,carrera)
+
+
